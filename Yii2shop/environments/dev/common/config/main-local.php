ceb7288e000d8e1013183f0c7509f93fa8686526
Plakhotnyi Oleksandr <plahotniy.alex@gmail.com>
2022-08-08 16:58:08 +0300

    'AdminLTEinstalled'

--- Yii2shop/environments/dev/common/config/main-local.php
+++ Yii2shop/environments/dev/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=mysql;dbname=shop',
             'username' => 'root',
-            'password' => '',
+            'password' => 'root',
             'charset' => 'utf8',
         ],
         'mailer' => [
--- Yii2shop/environments/dev/common/config/params-local.php
+++ Yii2shop/environments/dev/common/config/params-local.php
@@ -1,4 +1,5 @@
 <?php
-
 return [
+    'cookieValidationKey' => '',
+    'cookieDomain' => '.shop.d',
 ];
